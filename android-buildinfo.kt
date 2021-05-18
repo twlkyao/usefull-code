e722a360c3d5c3fed614ce84112c5baade200f20
twlkyao <qishiyao2008@126.com>
2021-05-18 17:03:26 +0800

    Create android-buildinfo.kt

---
+++ android-buildinfo.kt
@@ -0,0 +1,7 @@
+ Log.d(TAG, "\nmodel=${Build.MODEL}" +
+          "\nbrand=${Build.BRAND}" +
+          "\ndeivce=${Build.DEVICE}" +
+          "\nmanufacture=${Build.MANUFACTURER}" +
+          "\nproduct=${Build.PRODUCT}" +
+          "\ntype=${Build.TYPE}" +
+          "\nuser=${Build.USER}")
